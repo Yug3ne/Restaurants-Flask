26f1ac1c83f0b00a64485c679d093880a616166f
Koech <eugenekoech9@gmail.com>
2024-04-09 09:18:27 +0300

    created relationships

--- server/models.py
+++ server/models.py
@@ -2,14 +2,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 
 
+
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=True)
     address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('Restaurant_Pizza', backref='restaurant')
 
 class Pizza(db.Model):
     __tablename__ = 'pizzas'
@@ -18,16 +21,21 @@ class Pizza(db.Model):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-class RestaurantPizza(db.model):
+    restaurant_pizzas = db.relationship('Restaurant_Pizza', backref='restaurant')
+
+
+class Restaurant_Pizza(db.model):
     __tablename__ = 'restaurant_pizzas'
 
     id=  db.Column(db.Integer, primary_key=True)
+    price= db.Column(db.Float, nullable=False)
     pizza_id= db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
     restaurant_id= db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
-    price= db.Column(db.Float, nullable=False)
+
+
 
     @validates
     def validate_price(self, price):
         if not (1 <= price <= 30):
             raise ValueError("Price must be between 1 and 30")
-        return price
+        return price 
